79aabcbf9c927f23679e32738974a323d043e5da
Jake Wharton <jw@squareup.com>
2013-07-03 16:35:50 -0700

    Remove craziness around native tag bug line regex.
    Conflicts:
    	pidcat.py

--- regex.py
+++ regex.py
@@ -1,7 +1,7 @@
 import re
 
 LOG_LINE = re.compile(r'^([A-Z])/([^\(]+)\( *(\d+)\): (.*)\r?$')
-BUG_LINE = re.compile(r'^(?!.*(nativeGetEnabledTags)).*$')
+BUG_LINE = re.compile(r'.*nativeGetEnabledTags.*')
 START_LINE = re.compile(r'^I/am_proc_start\( *\d+\): \[0?,?(\d+),(\d+),([a-zA-Z0-9._:]+),.*,(.*)\]\r?$')
 DEATH_LINE = re.compile(r'^I/am_proc_died\( *\d+\): \[0?,?(\d+),([a-zA-Z0-9._:]+),?\d*\]\r?$')
 KILL_LINE = re.compile(r'^I/am_kill ?\( *\d+\): \[0?,?(\d+),([a-zA-Z0-9._:]+),.*\]\r?$')
